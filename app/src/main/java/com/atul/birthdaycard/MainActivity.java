2bc4f16d54e6d8c933375da3488e4e627dbfd8f8
Atul Pratap Singh <pratapsinghatul111@gmail.com>
2022-10-01 23:57:14 +0530

    Birthday Card

--- app/src/main/java/com/atul/birthdaycard/MainActivity.java
+++ app/src/main/java/com/atul/birthdaycard/MainActivity.java
@@ -3,12 +3,30 @@ package com.atul.birthdaycard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btn;
+    private EditText edit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.button);
+        edit = findViewById(R.id.editTextTextPersonName);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = edit.getEditableText().toString();
+                Toast.makeText(MainActivity.this,
+                        "Button is "+name,
+                        Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
